74391c43c8e2d3a82fd7a331185ee2dd10ca67f8
BJQRS <162879554+BJQRS@users.noreply.github.com>
2024-03-25 08:03:10 +0000

    add requirements

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template,request
+from database import load_projects_from_db, load_project_from_db,add_message_to_db
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-  return '<h1>Majed Almutairi</h1>'
+  projects = load_projects_from_db()
+  return render_template('home.html', projects=projects)
 
-if __name__ == '__main__':
+
+@app.route("/project/<id>")
+def project_details(id):
+  project = load_project_from_db(id)
+  return render_template('project_details.html', project=project)
+
+
+@app.route("/massage")
+def massage():
+  return render_template('contactus.html')
+
+@app.route("/massage_submitted",methods=['POST'])
+def massage_submitted():
+  data = request.form
+  add_message_to_db(data)
+  return render_template('massage_submitted.html',data=data)
+  
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
-
